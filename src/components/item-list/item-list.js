cefa284d59702188c5b8bf7d6612df886f952af1
Lyuda Barabash <barabashlyuda@gmail.com>
2019-10-19 21:32:49 +0300

    Add pink line

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -7,8 +7,12 @@ import Item from '../item/item.js';
 const ItemList = () =>{
   return (
     <div className="item-list-wrapper">
-      <Item text="First item with custom name" number="132" />
-      <Item text="Second  item is active" number="3" />
+      <Item
+        text="First item with custom name"
+        number="132"
+        active={false}
+       />
+      <Item text="Second  item is active" number="3" active={true} />
     </div>
   );
 }
--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './item.scss';
 
 import Number from '../number/number.js';
 import DeleteButton from '../delete-button/delete-button.js';
 
-const Item = (props) =>{
+const Item = (props) => {
+  const lineClass = classNames({
+    'pink-line': true,
+    'pink-line-active': props.active
+  });
+
   return (
-    <div className="item-wrapper">
-      <div className="text-wrapper">
-        <p className="item-text">{props.text}</p>
-        <Number number={props.number} />
+    <div className="row-wrapper">
+      <div className={lineClass}></div>
+      <div className="item-wrapper">
+        <div className="text-wrapper">
+          <p className="item-text">{props.text}</p>
+          <Number number={props.number} />
+        </div>
+        <DeleteButton text="Delete" />
       </div>
-      <DeleteButton text="Delete" />
     </div>
   );
 }
