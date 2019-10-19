18569914a43b89e17b1519694a1d67c214b0bc65
Lyuda Barabash <barabashlyuda@gmail.com>
2019-10-19 16:08:38 +0300

    Create title component

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -4,10 +4,14 @@ import './comments.scss';
 
 import CommentList from '../comment-list/comment-list.js';
 import AddComment from '../add-comment/add-comment.js';
+import Title from '../title/title.js';
 
 const Comments = () =>{
   return (
     <div className="comments-wrapper">
+      <Title
+        titleText="Comments #2"
+      />
       <CommentList />
       <AddComment />
     </div>
---
+++ src/components/title/title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './title.scss';
+
+const Title = (props) =>{
+  return (
+    <div className="title-wrapper">
+      <h1>{props.titleText}</h1>
+    </div>
+  );
+}
+
+export default Title;
