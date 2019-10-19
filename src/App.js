c82aa36691e4df6224cee341af5e21c615ff0bbe
Lyuda Barabash <barabashlyuda@gmail.com>
2019-10-19 12:57:47 +0300

    Create sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 
+import Sidebar from './components/sidebar.js';
+
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './sidebar.scss';
+
+const Sidebar = () =>{
+  return (
+    <div className="sidebar-wrapper">
+      <h1>Dairy app</h1>
+      <p className="description">Comment with no sense</p>
+    </div>
+  );
+}
+
+export default Sidebar;
