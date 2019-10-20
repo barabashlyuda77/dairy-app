3e0996c6d07211e7b9d6fcd2ecefa7f735bbb27e
Lyuda Barabash <barabashlyuda@gmail.com>
2019-10-20 15:55:49 +0300

    Add bacis onclick functionality to add button

--- src/components/add-button/add-button.js
+++ src/components/add-button/add-button.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './add-button.scss';
 
-const AddButton = () =>{
+const AddButton = (props) => {
   return (
-    <div className="add-button-wrapper">
+    <div className="add-button-wrapper" onClick={props.onClick}>
       <p className="button-text">Add new</p>
     </div>
   );
--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './add-item.scss';
 
 import AddButton from '../add-button/add-button.js';
 
-const AddItem = () =>{
-  return (
-    <div className="add-item-wrapper">
-      <input
-        className="input-text"
-        type="text"
-        placeholder="Type name here..."
-      >
-      </input>
-      <AddButton />
-    </div>
-  );
+class AddItem extends Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef()
+  }
+
+  createItem = (value) => {
+    return {
+      id: Date.now(),
+      title: value,
+      active: false,
+      comments: []
+    };
+  }
+
+  getItemsFromLocalStorage = () => {
+    const localStorageItems = localStorage.getItem('items');
+    return localStorageItems === null ? [] : JSON.parse(localStorageItems);
+  }
+
+  saveItemsToLocalStorage = (items) => {
+    localStorage.setItem('items', JSON.stringify(items));
+  }
+
+  addItemToItemsList = (title) => {
+    const items = this.getItemsFromLocalStorage();
+    const item = this.createItem(title);
+    items.push(item);
+    this.saveItemsToLocalStorage(items);
+  }
+
+  clickHandler = () => {
+    const title = this.textInput.current.value;
+    this.addItemToItemsList(title);
+
+    console.log(JSON.parse(localStorage.getItem('items')));
+
+    //display data to item list
+  }
+
+  render() {
+    return (
+      <div className="add-item-wrapper">
+        <input
+          className="input-text"
+          type="text"
+          placeholder="Type name here..."
+          ref={this.textInput}
+        />
+        <AddButton onClick={this.clickHandler}/>
+      </div>
+    );
+  }
 }
 
 export default AddItem;
