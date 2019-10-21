6e81abef622a386bb050bc43629e03b8fab3c0e5
Lyuda Barabash <barabashlyuda@gmail.com>
2019-10-21 13:47:42 +0300

    Updates

---
+++ precache-manifest.d75611cb031905fab9a998ae2653d510.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841d9c8870178174ee11e3ce411bf900",
+    "url": "/dairy-app/index.html"
+  },
+  {
+    "revision": "5ff1a9f5384c47d974e3",
+    "url": "/dairy-app/static/css/main.1f3fa87a.chunk.css"
+  },
+  {
+    "revision": "0fb3bc7b860d7175b0b8",
+    "url": "/dairy-app/static/js/2.732fa763.chunk.js"
+  },
+  {
+    "revision": "5ff1a9f5384c47d974e3",
+    "url": "/dairy-app/static/js/main.27a0baa4.chunk.js"
+  },
+  {
+    "revision": "3438d13f0f295786041f",
+    "url": "/dairy-app/static/js/runtime-main.cc4b138d.js"
+  }
+]);
